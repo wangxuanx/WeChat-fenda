5af4c1a8346d05a53fc47b522eecda75c2ddf622
GuanLin <137802892@qq.com>
2019-06-24 17:48:53 +0800

    Save login userInfo in the cloud

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -5,12 +5,41 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  const {
+    OPENID,
+    APPID,
+    UNIONID,
+    ENV,
+  } = cloud.getWXContext()
+  const userCollection = cloud.database().collection('user')
+  const userInfo = event.userInfo
+
+  // 判断用户是否存在
+  const result = await userCollection.where({
+    openid: OPENID,
+  })
+  if(result.count().total==0)
+  {
+    // 不存在则添加用户
+    userCollection.add({
+      data: {
+        avatarUrl: userInfo.avatarUrl,
+        fan_list: [],
+        follow_list: [],
+        nickName: userInfo.nickName,
+        openid: OPENID
+      }, success: res => {
+        console.log(res)
+      }, fail: err => {
+        console.log(err)
+      }
+    })
+  }
 
   return {
     event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    openid: OPENID,
+    appid: APPID,
+    unionid: UNIONID,
   }
 }
