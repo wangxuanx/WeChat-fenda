df704bb4ea62f6fd3df19096060dcce1d03a2995
GuanLin <137802892@qq.com>
2019-06-25 14:19:55 +0800

    6.25 14:19

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -13,44 +13,40 @@ exports.main = async (event, context) => {
   } = cloud.getWXContext()
   const userCollection = cloud.database().collection('user')
   const userInfo = event.userInfo
+  var debugInfo = []
 
   // 判断用户是否存在
-  try{
-    await userCollection.where({
-      openid: OPENID,
-    }).get({
-      success: res => {
-        if (res.data.length == 0) {
-          console.log("New user, add into db.")
-          // 不存在则添加用户
-          userCollection.add({
-            data: {
-              avatarUrl: userInfo.avatarUrl,
-              desc: "写点东西介绍自己吧！",
-              fan_list: [],
-              fan_num: 0,
-              follow_lis: [],
-              follow_num: 0,
-              nickName: userInfo.nickName,
-              openid: OPENID
-            }, success: res => {
-              console.log(res)
-            }, fail: err => {
-              console.log(err)
-            }
-          })
-        }
+  userCollection.where({
+    _openid: OPENID
+  }).get().then(res => {
+      debugInfo.push("get success\n")
+      if (res.data.length == 0) {
+        debugInfo.push("New user, add into db.\n")
+        // 不存在则添加用户
+        userCollection.add({
+          data: {
+            avatarUrl: userInfo.avatarUrl,
+            desc: "写点东西介绍自己吧！",
+            fan_list: [],
+            fan_num: 0,
+            follow_list: [],
+            follow_num: 0,
+            nickName: userInfo.nickName,
+          }, success: res => {
+            debugInfo.push("add success\n")
+          }, fail: err => {
+            debugInfo.push("add fail\n")
+          }
+        })
       }
     }
-    )
-  } catch(e){
-    console.error(e)
-  }
-  
+  )
+
   return {
     event,
     openid: OPENID,
     appid: APPID,
     unionid: UNIONID,
+    debugInfo
   }
 }
