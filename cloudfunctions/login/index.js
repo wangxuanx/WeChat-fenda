2da4ad66b5c98254fc592b4cbb24ba7df8afb7f0
GuanLin <137802892@qq.com>
2019-06-25 09:54:26 +0800

    Save login userInfo in the cloud

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -13,32 +13,42 @@ exports.main = async (event, context) => {
   } = cloud.getWXContext()
   const userCollection = cloud.database().collection('user')
   const userInfo = event.userInfo
-
-  // 判断用户是否存在
-  const result = await userCollection.where({
+  const result = userCollection.where({
     openid: OPENID,
   })
-  if(result.count().total==0)
-  {
-    // 不存在则添加用户
-    userCollection.add({
-      data: {
-        avatarUrl: userInfo.avatarUrl,
-        desc: userInfo.desc,
-        fan_list: [],
-        fan_num: 0,
-        follow_list: [],
-        follow_num: 0,
-        nickName: userInfo.nickName,
-        openid: OPENID
-      }, success: res => {
-        console.log(res)
-      }, fail: err => {
-        console.log(err)
-      }
-    })
-  }
 
+  // 判断用户是否存在
+  try{
+    await userCollection.where({
+      openid: OPENID,
+    }).get({
+      success: res => {
+        if (res.length > 0) {
+          // 不存在则添加用户
+          userCollection.add({
+            data: {
+              avatarUrl: userInfo.avatarUrl,
+              desc: userInfo.desc,
+              fan_list: [],
+              fan_num: 0,
+              follow_list: [],
+              follow_num: 0,
+              nickName: userInfo.nickName,
+              openid: OPENID
+            }, success: res => {
+              console.log(res)
+            }, fail: err => {
+              console.log(err)
+            }
+          })
+        }
+      }
+    }
+    )
+  } catch(e){
+    console.error(e)
+  }
+  
   return {
     event,
     openid: OPENID,
