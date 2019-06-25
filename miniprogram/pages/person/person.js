db0c64029e1245e00326837dacaac0c04e6a9b45
StrongChenYu <jarvis.chenyu@gmail.com>
2019-06-25 08:45:56 +0800

    找人界面基本完成

--- miniprogram/pages/person/person.js
+++ miniprogram/pages/person/person.js
@@ -2,16 +2,55 @@
 //获取应用实例
 var app = getApp()
 Page({
+    
     data: {
-
+      userInfo:undefined
     },
     onLoad: function(e) {
-        console.log(e)
+      console.log(e.id);
+        //根据e.id发请求
+      var userId = e.id;
+      var _this = this;
+      this.setData({
+        userInfo:_this.testData[userId]
+      })
+      console.log(this.data.userInfo);
+        
     },
     toIssue: function(e) {
         console.log(e)
         wx.navigateTo({
             url: '../issue/issue?master=' + e.target.dataset.master
         })
-    }
+    },
+
+
+    testData: [{
+    id: 0,
+    name: "陈章鱼",
+    avater: "czy.png",
+    intro: "知乎50万关注答主，人丑就该多读书",
+    quizCost: 9.9,
+    detail: "每年至少读100本书，加200天班，还抽空追10部美剧，涨了5斤体重, 唯一能天天坚持的只有四件事：吃饭睡觉喘气读书",
+    fans: 2821,
+    if_follow: false
+  }, {
+    id: 1,
+    name: "马东",
+    avater: "md.png",
+    intro: "米未传媒CEO",
+    fans: 18967,
+    quizCost: 9.9,
+    detail: "每年至少读100本书，加200天班，还抽空追10部美剧，涨了5斤体重, 唯一能天天坚持的只有四件事：吃饭睡觉喘气读书",
+    if_follow: false
+  }, {
+    id: 2,
+    name: "张翼",
+    avater: "lcx.png",
+    intro: "猫奴+转业军官+电影/电视剧 演员",
+    fans: 3646,
+    quizCost: 9.9,
+    detail: "每年至少读100本书，加200天班，还抽空追10部美剧，涨了5斤体重, 唯一能天天坚持的只有四件事：吃饭睡觉喘气读书",
+    if_follow: false
+  }]
 })
--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,16 +1,20 @@
 Page({
     data: {
         hotMasters: [{
+            id:0,
             name: "陈章鱼",
             avater: "czy.png",
             intro: "知乎50万关注答主，人丑就该多读书",
-            fans: 2821
+            fans: 2821,
+            if_follow: false
         }, {
+            id:1,
             name: "马东",
             avater: "md.png",
             intro: "米未传媒CEO",
             fans: 18967
         }, {
+            id:2,
             name: "张翼",
             avater: "lcx.png",
             intro: "猫奴+转业军官+电影/电视剧 演员",
@@ -36,7 +40,45 @@ Page({
     onLoad: function() {
 
     },
-    toPerson: function() {
+    //跳转函数
+    toPerson: function(event) {
+      var targetUrl = '/pages/person/person?id=' + event.currentTarget.dataset.userId;
+      console.log(event);
+      wx.navigateTo({
+        url: targetUrl//实际路径要写全
+      })
+    },
 
+    handleFollowTap: function() {
+      console.log("handle follow");
+    },
+    handleFollowTap: function(event) {
+      var changeId = 'hotMasters[' + event.target.dataset.followId + '].if_follow';
+      this.setData({
+        [changeId]:false
+      })
+    },
+    handleNotFollowTap: function(event) {
+      //console.log("handle not follow");
+      //注意需要调动后端接口
+      var _this = this;
+      var _event = event;
+      
+      wx.showModal({
+        content: '确定要取消关注 ' + event.target.dataset.followName +' 吗？',
+        success(res) {
+          if (res.confirm) {
+            //console.log('用户点击确定');
+            var changeId = 'hotMasters[' + event.target.dataset.followId + '].if_follow';
+            _this.setData({
+              [changeId]: true
+            })
+            //调用服务器接口！
+          } else if (res.cancel) {
+            //do nothing
+            console.log('用户点击取消');
+          }
+        }
+      })
     }
 })
