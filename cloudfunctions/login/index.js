0a6405e07583aea7e511e51c4c6dd95173872062
GuanLin <137802892@qq.com>
2019-06-25 15:02:56 +0800

    云函数数据库功能移除

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -11,42 +11,46 @@ exports.main = async (event, context) => {
     UNIONID,
     ENV,
   } = cloud.getWXContext()
-  const userCollection = cloud.database().collection('user')
-  const userInfo = event.userInfo
-  var debugInfo = []
 
-  // 判断用户是否存在
-  userCollection.where({
-    _openid: OPENID
-  }).get().then(res => {
-      debugInfo.push("get success\n")
-      if (res.data.length == 0) {
-        debugInfo.push("New user, add into db.\n")
-        // 不存在则添加用户
-        userCollection.add({
-          data: {
-            avatarUrl: userInfo.avatarUrl,
-            desc: "写点东西介绍自己吧！",
-            fan_list: [],
-            fan_num: 0,
-            follow_list: [],
-            follow_num: 0,
-            nickName: userInfo.nickName,
-          }, success: res => {
-            debugInfo.push("add success\n")
-          }, fail: err => {
-            debugInfo.push("add fail\n")
-          }
-        })
-      }
-    }
-  )
+  
+  // const db = cloud.database()
+  // const userInfo = event.userInfo
+  // var debugInfo = []
+
+  // // 判断用户是否存在
+  // db.collection("user").where({
+  //   _openid: OPENID
+  // }).get({
+  //   success: res => {
+  //     debugInfo.push("get success\n")
+  //     if (res.data.length == 0) {
+  //       debugInfo.push("New user, add into db.\n")
+  //       // 不存在则添加用户
+  //       db.collection("user").add({
+  //         data: {
+  //           avatarUrl: userInfo.avatarUrl,
+  //           desc: "写点东西介绍自己吧！",
+  //           fan_list: [],
+  //           fan_num: 0,
+  //           follow_list: [],
+  //           follow_num: 0,
+  //           nickName: userInfo.nickName,
+  //         }, success: res => {
+  //           debugInfo.push("add success\n")
+  //         }, fail: err => {
+  //           debugInfo.push("add fail\n")
+  //         }
+  //       })
+  //     }
+  //   }
+  // })
+
 
   return {
     event,
     openid: OPENID,
     appid: APPID,
     unionid: UNIONID,
-    debugInfo
+    // debugInfo
   }
 }
