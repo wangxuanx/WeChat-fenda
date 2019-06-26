87796b795d7e5b1fc78e2b0df9f16b757b80c151
Jeleren <17771396576@163.com>
2019-06-26 16:27:40 +0800

    fix

--- miniprogram/pages/edit/edit.js
+++ miniprogram/pages/edit/edit.js
@@ -7,10 +7,7 @@ Page({
    */
   data: {
     images: [],
-<<<<<<< HEAD
     text: ''
-=======
->>>>>>> fenda
   },
 
 
@@ -159,45 +156,4 @@ Page({
       console.log(res.errCode)
     })
   },
-<<<<<<< HEAD
-=======
-  start: function () {
-    //开始录音
-    var options = {
-      duration: 10000,//指定录音的时长，单位 ms
-      sampleRate: 16000,//采样率
-      numberOfChannels: 1,//录音通道数
-      encodeBitRate: 96000,//编码码率
-      format: 'mp3',//音频格式，有效值 aac/mp3
-      frameSize: 50,//指定帧大小，单位 KB
-    }
-    recorderManager.start(options);
-    recorderManager.onStart(() => {
-      console.log('recorder start')
-    });
-    //错误回调
-    recorderManager.onError((res) => {
-      console.log(res);
-    })
-  },
-  stop: function () {
-    recorderManager.stop();
-    recorderManager.onStop((res) => {
-      this.tempFilePath = res.tempFilePath;
-      console.log('停止录音', res.tempFilePath)
-      const { tempFilePath } = res
-    })
-  },
-  play: function () {
-    innerAudioContext.autoplay = true
-    innerAudioContext.src = this.tempFilePath,
-      innerAudioContext.onPlay(() => {
-        console.log('开始播放')
-      })
-    innerAudioContext.onError((res) => {
-      console.log(res.errMsg)
-      console.log(res.errCode)
-    })
-  },
->>>>>>> fenda
 })
