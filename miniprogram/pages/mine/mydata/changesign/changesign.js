c915af46abf42aafbd318ebea2140ffb1b71dc05
GuanLin <137802892@qq.com>
2019-06-27 11:12:45 +0800

    修改个人签名数据管理

--- miniprogram/pages/mine/mydata/changesign/changesign.js
+++ miniprogram/pages/mine/mydata/changesign/changesign.js
@@ -1,15 +1,42 @@
 // miniprogram/pages/mine/mydata/changesign/changesign.js
+let app = getApp()
+
 Page({
   data:{
     usersign: ""             //获取到的用户签名
   },
 
-  bindFormsubmit: function (e) {         //从输入框获取签名
-    console.log(e.detail.value)
-    if ((e.detail.value.textarea) != "") {
-      this.setData({
-        usersign: e.detail.value
-      })
-    }
+  input_sign(e){
+    console.log(e)
+    this.setData({
+      usersign: e.detail.value
+    })
+  },
+
+  bindFormsubmit: function () {         //从输入框获取签名
+    wx.cloud.init()
+    let _this = this
+    const userCollection = wx.cloud.database().collection("user")
+    const _ = wx.cloud.database().command
+
+    userCollection.where({
+      _openid: app.globalData.userInfo._openid
+    }).get({
+      success: res => {
+        console.log(_this.data.usersign)
+        userCollection.doc(res.data[0]._id).update({
+          data:{
+            desc: _this.data.usersign
+          },
+          success: res => { 
+            console.log(res)
+            wx.navigateTo({
+              url: '/pages/mine/mydata/mydata',
+            })  
+          },
+          fail: res => { console.log(res)}
+        })
+      }
+    })
   }
 })
--- miniprogram/pages/mine/mydata/mydata.js
+++ miniprogram/pages/mine/mydata/mydata.js
@@ -8,14 +8,33 @@ Page({
   },
   onLoad: function () {            //获取全局数据
     console.log('onLoad')
-    var that = this
+    let _this = this
+    wx.cloud.init()
+    const userCollection = wx.cloud.database().collection("user")
+    const _ = wx.cloud.database().command
+
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function (userInfo) {
       //更新数据
-      that.setData({
+      _this.setData({
         userInfo: userInfo
       })
     })
+
+    // 获取用户签名
+    userCollection.where({
+      _openid: app.globalData.userInfo._openid
+    }).get({
+      success: res => {
+        _this.setData({
+          userSign: res.data[0].desc
+        })
+        console.log("更新签名成功")
+        console.log(_this.data)
+      },
+      fail: res => { console.log(res)}
+    })
+
   },
   changeSign: function() {             //修改签名函数
     wx.navigateTo({
